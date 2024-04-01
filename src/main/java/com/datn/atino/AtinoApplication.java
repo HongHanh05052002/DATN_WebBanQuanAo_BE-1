c1dc7c6684bcda69f81d83954101909bac34ba12
Dai-Trinh <97729352+Dai-Trinh@users.noreply.github.com>
2024-04-01 09:09:32 +0700

    Initial commit

---
+++ src/main/java/com/datn/atino/AtinoApplication.java
@@ -0,0 +1,13 @@
+package com.datn.atino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtinoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datn/atino/AtinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datn.atino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
